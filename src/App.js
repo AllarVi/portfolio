723a6c37363e3cf9397ad77e860ad0b357b10010
Allar <allar.viinamae@hotmail.com>
2019-09-07 18:04:14 +0300

    Change text and font color

--- src/App.js
+++ src/App.js
@@ -8,8 +8,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          Hi... My name is Allar. Just fiddling around.
+        </p>
+        <p>
           Edit <code>src/App.js</code> and save to reload.
-          Mina siin.
         </p>
         <a
           className="App-link"
